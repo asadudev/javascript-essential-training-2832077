ec2b4302f2bb8026b0ea19f6d8819bec63682807
Asadullah125-gif <asadaulakh089@gmail.com>
2023-06-20 16:57:30 +0500

    practice of object
    practice of object and nested object and how to access

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,27 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+
+const Person ={
+    SkinColor:"white",
+    HairColor:"Brown",
+    Height:"6feet",
+    EyeColor:"Black",
+    Weight:"85KG",
+    Student:{
+        Name:"Asad ullah",
+        University_RollNo:"021382",
+        Address:"Gujranwala",
+        Class:"Bscs"
+    }
+}
+
+
+console.log('This is Person Object:', Person)
+console.log('This is Nested Object:', Person.Student)
+
+console.log('University RollNo:',Person.Student.University_RollNo)
+console.log('Name:',Person["Student"]["Name"])
+console.log('Address:',Person["Student"]["Address"])
+
