ee6eed8df0bc5ce189b8877a0265b04f0f32cdfe
Asadullah125-gif <asadaulakh089@gmail.com>
2023-06-20 18:43:32 +0500

    method practice
    method practice

--- Practice/03_09/script.js
+++ Practice/03_09/script.js
@@ -16,6 +16,8 @@ const backpack = {
     left: 26,
     right: 26,
   },
+
+  lidOpen:false,
   toggleLid: function (lidStatus) {
     this.lidOpen = lidStatus;
   },
@@ -23,4 +25,31 @@ const backpack = {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
+  volumechange: function (newvolume){
+    this.volume = newvolume;
+  },
+  pocketNumchange: function (newpocketnum){
+    this.pocketNum = newpocketnum;
+  },
 };
+console.log('backpack:', backpack)
+
+console.log('befor volume:',backpack.volume);
+
+backpack.volumechange(34);
+
+console.log('after volume:',backpack.volume);
+
+
+console.log('old pocketnum:',backpack.pocketNum);
+
+backpack.pocketNumchange(21);
+
+console.log('new pocketnum:',backpack.pocketNum);
+
+console.log('befor right:',backpack.strapLength.right);
+
+backpack.newStrapLength(1,2);
+
+console.log('after right:',backpack.strapLength.right);
+
