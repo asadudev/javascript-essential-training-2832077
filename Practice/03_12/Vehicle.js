d56767624fb4d56703eea28cab8c342c189c27de
Asadullah125-gif <asadaulakh089@gmail.com>
2023-06-21 10:48:50 +0500

    practice of class
    create a class of vehicle and import in script.js

---
+++ Practice/03_12/Vehicle.js
@@ -0,0 +1,16 @@
+class Vehicle {
+    constructor(
+        Name,
+        color,
+        model,
+        number
+    ) {
+        this.Name = Name;
+        this.color = color;
+        this.model = model;
+        this.number = number
+    } 
+
+}
+
+export default Vehicle;
--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,20 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+
+import Vehicle from "./Vehicle.js";
+
+const Car = new Vehicle(
+    "Hondacity",
+    "Silver",
+    2021,
+    "ict1205"
+);
+
+console.log("vehicle object:",Vehicle);
+console.log("Vehicle name:", Car.Name);
+console.log("Vehicle color:", Car.color);
+console.log("Vehicle model:", Car.model);
+console.log("Vehicle number:", Car.number);
+
+
