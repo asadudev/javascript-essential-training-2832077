9f19e7179a9d6ca220d03c5db1cd5bc1afa8d55e
Asadullah125-gif <asadaulakh089@gmail.com>
2023-06-21 15:46:26 +0500

    practice find a element by queryselector
    find a element by queryselectorall and queryselector

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,8 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+
+document.querySelectorAll('main li')
+document.querySelectorAll('main li').forEach(item => item.style.background = 'red')
+document.querySelectorAll('main li:last-child').forEach(item => item.style.background = 'blue')
