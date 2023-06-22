6ec307eaa8fa07c0d420ca9fc33d202db75f6580
Asadullah125-gif <asadaulakh089@gmail.com>
2023-06-22 15:56:13 +0500

    practice of function
    practice of standard function and arrow function

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,47 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+
+const Vehicle = {
+    Name: 'Hondacity',
+    model: 2021,
+    color: 'Black',
+    owner_name: 'Asad'
+};
+
+// standard function
+// const Addvehicel = function(newvehicle){
+//     const NewArticle = document.createElement('article');
+//     NewArticle.innerHTML = `
+//     <h1>${newvehicle.Name}</h1>
+//     <ul>
+//         <li>${newvehicle.owner_name}</li>
+//         <li>${newvehicle.color}</li>
+//         <li>${newvehicle.model}</li>
+//     </ul>
+//     `;
+//     return NewArticle;
+// }
+
+// const Main = document.querySelector('main');
+// Main.append(Addvehicel(Vehicle));
+
+
+// With Arrow Function
+
+const Addvehicel = newvehicle => {
+    const NewArticle = document.createElement('article');
+    NewArticle.innerHTML = `
+    <h1>${newvehicle.Name}</h1>
+    <ul>
+        <li>${newvehicle.owner_name}</li>
+        <li>${newvehicle.color}</li>
+        <li>${newvehicle.model}</li>
+    </ul>
+    `;
+    return NewArticle;
+}
+
+const Main = document.querySelector('main');
+Main.append(Addvehicel(Vehicle));
